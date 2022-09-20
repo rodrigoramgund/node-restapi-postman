ce676605c8375e7cf16d2723e123e075e8893eca
rodrigoramgund <112991755+rodrigoramgund@users.noreply.github.com>
2022-09-20 19:51:25 -0300

    Initial files

---
+++ app.js
@@ -0,0 +1,104 @@
+// const express = require('express');
+// const res = require('express/lib/response');
+
+// const app = express();
+
+// app.get("/", (req, res) => {
+//     res.send("Introdução a API");
+// });
+
+// app.listen(8080, () => {
+//     console.log("Servidor iniciado na porta 8080: http://localhost:8080");
+// });
+
+// const express = require('express');
+// const res = require('express/lib/response');
+
+// const app = express();
+// const port = 8080;
+
+// app.use(express.json());
+
+// let movies = [
+//     {
+//         id: "1",
+//         titulo: "Inception",
+//         diretor: "Christopher Nolan",
+//         lancamento: "16/07/2010",
+//     },
+//     {
+//         id: "2",
+//         titulo: "The Irishman",
+//         diretor: "Martin Scorsese",
+//         lancamento: "27/09/2019",
+//     },
+// ];
+
+// app.get("/movie", (req, res) => {
+//     res.json(movies);
+// });
+
+// app.listen(port, () => console.log("Servidor iniciado na porta ${port}"));
+
+const express = require('express');
+const res = require('express/lib/response');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+let movies = [
+    {
+        id: "1",
+        titulo: "Inception",
+        diretor: "Christopher Nolan",
+        lancamento: "16/07/2010",
+    },
+    {
+        id: "2",
+        titulo: "The Irishman",
+        diretor: "Martin Scorsese",
+        lancamento: "27/09/2019",
+    },
+];
+
+app.get("/movie", (req, res) => {
+    res.json(movies);
+});
+
+app.post('/movie', (req, res) => {
+    const movie = req.body
+
+    console.log(movie);
+    movies.push(movie);
+    res.send('Filme adicionado a lista!');
+});
+
+app.get('/movie/:id', (req,res) => {
+    const id = req.params.id
+
+    for (let movie of movies) {
+        if(movie.id === id) {
+            res.json(movie)
+            return
+        }
+    }
+    res.status(404).send('movie not found!')
+})
+
+app.delete('/movie/:id', (req,res) => {
+    const id = req.params.id
+
+    movies = movies.filter(movie => {
+        if (movie.id !== id) {
+            return true;
+        }
+        return false;
+    });
+    res.send("FIlme foi apagado!");
+});
+
+app.listen(8080, () => console.log('Servidor iniciado na porta 8080'));
