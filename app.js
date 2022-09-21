61b1e2f9e1eb0e1bb5756ea11d357b04bce65446
rodrigoramgund <112991755+rodrigoramgund@users.noreply.github.com>
2022-09-20 21:39:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -101,4 +101,4 @@ app.delete('/movie/:id', (req,res) => {
     res.send("FIlme foi apagado!");
 });
 
-app.listen(8080, () => console.log('Servidor iniciado na porta 8080'));
+app.listen(port, () => console.log('Servidor iniciado na porta ${port}'));
